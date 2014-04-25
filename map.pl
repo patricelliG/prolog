13dbf06b1c225e1ddc5d9261379e43e1e6c88540
Gary Patricelli <garypatricelli@gmail.com>
2014-04-24 22:31:19 -0400

    init

---
+++ map.pl
@@ -0,0 +1,31 @@
+% Facts
+
+% Available color unique permentations
+% red, green, blue, yellow
+colors(red, green).
+colors(red, blue).
+colors(red, yellow).
+color(green, red).
+color(green, blue).
+color(green, yellow).
+color(blue, red).
+color(blue, green).
+color(blue, yellow).
+color(yellow, red).
+color(yellow, green).
+color(yellow, blue).
+
+% Rules 
+
+% Calculate map coloring
+color_map(A, B, C, D, E, F) :-
+color(A,B),
+color(A,C),
+color(A,D),
+color(C,D),
+color(D,E),
+color(C,E),
+color(F,E),
+color(C,F),
+color(C,B),
+color(F,B).
---
+++ people.pl
@@ -0,0 +1,27 @@
+% Assumptions
+
+female(alex).
+female(mary).
+male(carl).
+male(joe).
+
+married(mary, carl).
+has_job(mary).
+biology_major(joe).
+watching_tv(joe).
+
+
+
+% Rules
+happy(X) :- watching_tv(X) ; (is_married(X) , (gets_sleep(X))).
+
+busy(X) :- biology_major(X); has_job(X); has_children(X).
+
+gets_sleep(X) :-  \+ ((biology_major(X),has_job(X)) ;
+                     (biology_major(X),has_children(X)) ; 
+                     (has_job(X),has_children(X));
+                     (X == alex)).
+
+has_children(X,Y) :- married(X,Y).
+
+is_married(X) :- married(X,Y) ; married(Y,X).
